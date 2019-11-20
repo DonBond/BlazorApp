d69731786e3f780b48f2d873460dca569694cb68
Donovan <jetdon1@gmail.com>
2019-11-20 22:20:52 +0200

    blazor test app
    blazor test app

---
+++ BlazorFiddleProject/wwwroot/js/main.js
@@ -0,0 +1,7 @@
+﻿function switchGray() {
+    document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+function switchWhite() {
+    document.documentElement.setAttribute('data-theme', 'light');
+}
